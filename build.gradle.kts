b7e5fa196225b935ee7f420ee6e7932885b20cb9
Roger Viñas <roger.vinas@n26.com>
2022-06-09 12:09:42 +0200

    Set version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,24 +2,27 @@ plugins {
     id("org.ajoberstar.grgit") version "4.1.1"
 }
 
+group = "com.rogervinas"
+version = run {
+    val commitId = grgit.head().abbreviatedId
+    val tag = grgit.describe {
+        tags = true
+        always = true
+        longDescr = true
+    }
+    return@run Regex("(?<tag>.+)-(?<commits>\\d+)-g$commitId").matchEntire(tag)
+        ?.let {
+            if (it.groups["commits"]!!.value == "0") {
+                it.groups["tag"]!!.value
+            } else {
+                "${it.groups["tag"]!!.value}.$commitId"
+            }
+        }
+        ?: "0.0.0.$commitId"
+}
+
 tasks.register("getVersion") {
     doFirst {
-        val commitId = grgit.head().abbreviatedId
-        val tag = grgit.describe {
-            tags = true
-            always = true
-            longDescr = true
-        }
-        Regex("(?<tag>.+)-(?<commits>\\d+)-g$commitId").matchEntire(tag)
-            ?.let {
-                if (it.groups["commits"]!!.value == "0") {
-                    println("RELEASED VERSION ${it.groups["tag"]!!.value}")
-                } else {
-                    println("SNAPSHOT VERSION ${it.groups["tag"]!!.value}.$commitId")
-                }
-            }
-            ?: run {
-                println("SNAPSHOT VERSION 0.0.0-$commitId")
-            }
+        println("$version")
     }
 }
